0387fed3d2f9a8059207cd65924b8504e9417079
smollMushroom <fadhilahfarhan2003@gmail.com>
2025-05-13 15:04:45 +0700

    Remove dist folder from Git tracking

--- dist/config/roles.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.roles = void 0;
-exports.roles = {
-    warga: ['warga', 'admin', 'ketua'],
-    pengurus: ['admin', 'ketua'],
-    pemimpin: ['ketua'],
-};
--- dist/validators/letterRequest/createLetterRequestSchema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createLetterRequestSchema = void 0;
-const zod_1 = require("zod");
-exports.createLetterRequestSchema = zod_1.z.object({
-    letterType: zod_1.z.string().min(1, 'Jenis surat harus diisi'),
-    reason: zod_1.z.string().min(5, 'Alasan minimal 5 karakter'),
-});
